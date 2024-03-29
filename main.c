062ad1a9df1d11d24732fdfecb40b65145259bd1
agathemczk <agathe.maciaszczyk@icloud.com>
2024-03-29 12:18:20 +0100

    orange

--- main.c
+++ main.c
@@ -74,6 +74,7 @@ type_of_object BANANA = {"Banana", {215, 211, 50, 255}, 25};
 type_of_object STRAWBERRY = {"Strawberry", {255, 136, 166, 255}, 25};
 type_of_object IPAD = {"Ipad", {183, 154, 161, 255}, 25};
 type_of_object BLUEBERRY = {"Blueberry", {66, 49, 156, 255}, 25};
+type_of_object ORANGE = {"Orange", {231, 159, 88, 255}, 25};
 
 typedef struct object {
     type_of_object* type;
@@ -186,7 +187,7 @@ void render_menu(SDL_Renderer* renderer, TTF_Font* font, box* boxes) {
     SDL_SetRenderDrawColor(renderer, 95, 78, 70, 255);
     SDL_RenderFillRect(renderer, &menu_rect);
 
-    type_of_object* types[] = {&APPLE, &KIWI, &BANANA, &STRAWBERRY, &IPAD, &BLUEBERRY};
+    type_of_object* types[] = {&APPLE, &KIWI, &BANANA, &STRAWBERRY, &IPAD, &BLUEBERRY, &ORANGE};
     for(int i = 0; i < sizeof(types) / sizeof(types[0]); i++) {
         int total_count = 0;
         for(int j = 0; j < 6; j++) {
@@ -275,6 +276,10 @@ int main(int argc, char* argv[]) {
     add_object(&boxes[5],&BLUEBERRY);
     add_object(&boxes[5],&BLUEBERRY);
     add_object(&boxes[5],&BLUEBERRY);
+
+    for (int i = 0; i < 10; ++i) {
+        add_object(&boxes[4], &ORANGE);
+    }
 
 
     for (int i = 0; i < 6; ++i) {
